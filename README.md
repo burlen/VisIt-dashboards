4cf3e047bd1fe9a4c2af631d0cec09f54524ca56
Burlen Loring <bloring@lbl.gov>
2013-10-17 17:20:39 -0700

    missmarple: refined configuration

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 VisIt-dashboards
 ================
+A collection of scripts that configure and run VisIt regression tests.
 
-A collection of scripts that configure and run VisIt regression tests
+Usage
+-----
+1. create a "root" directory where your dashboard will live
+2. in the root directory create 3 subdirectories 
+2. * root/visit-src
+2. * root/visit-deps
+2. * root/visit-build
+3. copy the "visit" dir(where all the compiled libraries are) from a recent run of build_visit into root/visit-deps
+4. modify the paths and options in *missm-config.cmake* 
+5. modify the run environment and command line in *missm-test.sh*
+6. setup a cron job to run the dashboard
+
+
+
--- missm-config.cmake
+++ missm-config.cmake
@@ -1,35 +1,53 @@
+# expects the following directories
+# CTEST_DASHBOARD_ROOT
+#       /visit-deps
+#       /visit-src
+#       /visit-build
 set(CTEST_PROJECT_NAME "VisIt")
-set(NERSC_USER loring)
+#-----------------------------------------------------------------------------
+# site specific config
 set(CTEST_DASHBOARD_ROOT "/work3/visit-branch/dashboard/")
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(NERSC_USER loring)
+set(CONFIG_SITE ${CTEST_DASHBOARD_ROOT}/visit-deps/missmarple.cmake)
 set(CTEST_SITE "missmarple.lbl.gov")
 set(CTEST_BUILD_NAME "linux-x86_64_gcc-4.6-g")
+set(TEST_WARNINGS "-Wall -Wno-unused-function -Wno-sign-compare -Wno-unused-variable -Wno-write-strings -Wno-parentheses -Wno-unused-but-set-variable -Wno-missing-braces")
+#set($ENV{LC_MESSAGES} "en_US")
+#-----------------------------------------------------------------------------
+# general config
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_SOURCE_DIRECTORY "${CTEST_DASHBOARD_ROOT}/visit-src")
 set(CTEST_BINARY_DIRECTORY "${CTEST_DASHBOARD_ROOT}/visit-build")
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
 set(CTEST_BUILD_FLAGS -j32)
 set(CTEST_BUILD_CONFIGURATION Debug)
 if (NOT CTEST_TEST_TIMEOUT)
-  set(CTEST_TEST_TIMEOUT 120)
+    set(CTEST_TEST_TIMEOUT 120)
 endif()
 set(CTEST_SVN_COMMAND "/usr/bin/svn")
 set(CTEST_SVV_CHECKOUT
-  "${CTEST_SVN_COMMAND} co svn+ssh://${NERSC_USER}@portal-auth.nersc.gov/project/projectdirs/visit/svn/visit/trunk/src visit-src/")
+    "${CTEST_SVN_COMMAND} co svn+ssh://${NERSC_USER}@portal-auth.nersc.gov/project/projectdirs/visit/svn/visit/trunk/src visit-src/")
 set(CTEST_UPDATE_COMMAND "${CTEST_SVN_COMMAND}")
-set(CTEST_CUSTOM_MAXIMUM_NUMBER_OF_WARNINGS 2048)
-#set($ENV{LC_MESSAGES} "en_US")
+#-----------------------------------------------------------------------------
+# run the tests
 ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt"
-"
-VISIT_CONFIG_SITE:STRING=${CTEST_DASHBOARD_ROOT}/visit-deps/missmarple.cmake
-BUILDNAME:STRING=linux-x86_64_gcc-4.6-g
-SITE:STRING=missmarple.lbl.gov
+"VISIT_CONFIG_SITE:STRING=${CONFIG_SITE}
 CMAKE_BUILD_TYPE=Debug
-CMAKE_CXX_FLAGS=-Wall
+CMAKE_CXX_FLAGS=${TEST_WARNINGS}
 ")
 ctest_start("Experimental")
-ctest_update(SOURCE ${CTEST_SOURCE_DIRECTORY})
-ctest_configure()
-ctest_build()
-ctest_test()
-ctest_submit()
+set(numUpdated 0)
+ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" RETURN_VALUE numUpdated)
+if (numUpdated GREATER 0)
+    message(STATUS "${numUpdated} files changed in the source tree")
+    ctest_configure()
+    ctest_read_custom_files("${CTEST_BINARY_DIRECTORY}")
+    ctest_build()
+    ctest_test()
+    ctest_submit()
+elseif (numUpdated LESS 0)
+    message(STATUS "ERROR failed to update the sources")
+else()
+    message(STATUS "No files changed since the last dashboard run.")
+endif()
