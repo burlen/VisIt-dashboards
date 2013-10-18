5ef3b67148b4f51240e32defce6e22d4ffe1d0a4
Burlen Loring <bloring@lbl.gov>
2013-10-17 18:00:00 -0700

    missmarple: added option for continuous submission

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-VisIt-dashboards
-================
+# VisIt-dashboards
 A collection of scripts that configure and run VisIt regression tests.
 
-Usage
------
+## Basic Usage
 1. create a "root" directory where your dashboard will live
 2. in the root directory create 3 subdirectories 
 2. * root/visit-src
 2. * root/visit-deps
 2. * root/visit-build
 3. copy the "visit" dir(where all the compiled libraries are) from a recent run of build_visit into root/visit-deps
-4. modify the paths and options in *missm-config.cmake* 
-5. modify the run environment and command line in *missm-test.sh*
+4. modify the paths and options in *missm-config.cmake*. Consider renaming the file to <hostname>-config.cmake. 
+5. modify the run environment and command line in *missm-test.sh*. Consider renaming the file to <hostname>-test.sh. 
 6. setup a cron job to run the dashboard
 
 
+##Advanced Usage
+To update the dashboard on new commits. Set up a cron job to fire on a regular interval(eg 30 min) and in *missm-test.sh* set:
 
+```cmake
+ALWAYS_UPDATE=OFF
+INCREMENTAL_BUILD=ON
+```
--- missm-config.cmake
+++ missm-config.cmake
@@ -12,6 +12,8 @@ set(CONFIG_SITE ${CTEST_DASHBOARD_ROOT}/visit-deps/missmarple.cmake)
 set(CTEST_SITE "missmarple.lbl.gov")
 set(CTEST_BUILD_NAME "linux-x86_64_gcc-4.6-g")
 set(TEST_WARNINGS "-Wall -Wno-unused-function -Wno-sign-compare -Wno-unused-variable -Wno-write-strings -Wno-parentheses -Wno-unused-but-set-variable -Wno-missing-braces")
+set(ALWAYS_UPDATE ON)
+set(INCREMENTAL_BUILD OFF)
 #set($ENV{LC_MESSAGES} "en_US")
 #-----------------------------------------------------------------------------
 # general config
@@ -30,16 +32,17 @@ set(CTEST_SVV_CHECKOUT
 set(CTEST_UPDATE_COMMAND "${CTEST_SVN_COMMAND}")
 #-----------------------------------------------------------------------------
 # run the tests
-ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
-file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt"
+if (NOT INCREMENTAL_BUILD)
+    ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+    file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt"
 "VISIT_CONFIG_SITE:STRING=${CONFIG_SITE}
 CMAKE_BUILD_TYPE=Debug
-CMAKE_CXX_FLAGS=${TEST_WARNINGS}
-")
+CMAKE_CXX_FLAGS=${TEST_WARNINGS}")
+endif()
 ctest_start("Experimental")
 set(numUpdated 0)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}" RETURN_VALUE numUpdated)
-if (numUpdated GREATER 0)
+if (ALWAYS_UPDATE OR (numUpdated GREATER 0))
     message(STATUS "${numUpdated} files changed in the source tree")
     ctest_configure()
     ctest_read_custom_files("${CTEST_BINARY_DIRECTORY}")
