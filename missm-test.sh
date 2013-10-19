caf56472e8917fcf4b6c10cd1b095f6ae0a49422
Burlen Loring <bloring@lbl.gov>
2013-10-18 18:55:01 -0700

    missmarple: Updated config and exec. Updated README

--- missm-test.sh
+++ missm-test.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/bash -l
 module load mpich-3.0.3-R
 module load cmake-2.8.10
 module load qt-4.8.2
-module load ninja
-ctest -S /work3/visit-branch/dashboard/missm-config.cmake -O missm-test.log -V
+logfileId=`date +%s`
+cd /work3/visit-branch/dashboard
+ctest -S /work3/visit-branch/dashboard/missm-config.cmake -O missm-test-$logfileId.log -V
+find /work3/visit-branch/dashboard -name 'missm-test*.log' -atime 3 -exec rm \{\} \;
