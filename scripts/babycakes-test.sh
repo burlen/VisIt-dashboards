f5fc78f4d210ebb890eb92900b36cef22c5c20fc
Burlen Loring <bloring@lbl.gov>
2013-10-20 23:19:52 -0700

    babycakes: put logs ina subdir

--- scripts/babycakes-test.sh
+++ scripts/babycakes-test.sh
@@ -10,5 +10,5 @@ fi
 source /Users/bloring/openmpi-1.6.4.sh
 export DASHBOARD_TYPE=$1
 EPOCH=`date +%s`
-ctest -S /Users/bloring/visit/dashboards/babycakes-config.cmake -O $DASHBOARD_TYPE-$EPOCH.log -V
-find /work2/visit-branch/dashboard -maxdepth 0 -name '*.log' -atime 2 -exec rm \{\} \;
+ctest -S /Users/bloring/visit/dashboards/babycakes-config.cmake -O ./logs/$DASHBOARD_TYPE-$EPOCH.log -V
+find /Users/bloring/visit/dashboards/logs -maxdepth 0 -name '*.log' -atime 2 -exec rm \{\} \;
