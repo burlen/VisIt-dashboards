0c521fae7e6dc21c3efcb48bb5cebed6b92c0591
Burlen Loring <bloring@lbl.gov>
2014-02-01 10:33:11 -0800

    babycakes: use new deps builds

--- scripts/babycakes-config.cmake
+++ scripts/babycakes-config.cmake
@@ -1,7 +1,7 @@
 set(CTEST_SITE "babycakes.lbl.gov")
 set(CTEST_BUILD_NAME "MacOSX10.8.2-gcc4.2.1")
 set(CTEST_DASHBOARD_ROOT "/Users/bloring/visit/dashboards")
-set(CONFIG_SITE ${CTEST_DASHBOARD_ROOT}/visit-deps/babycakes.cmake)
+set(CONFIG_SITE ${CTEST_DASHBOARD_ROOT}/visit-deps-all/babycakes-deps-all.cmake)
 set(TEST_WARNINGS "-Wall -Wno-overloaded-virtual -Wno-unused-function -Wno-sign-compare -Wno-unused-variable -Wno-write-strings -Wno-parentheses -Wno-missing-braces")
 set(INITIAL_CACHE
 "VISIT_CONFIG_SITE:STRING=${CONFIG_SITE}
