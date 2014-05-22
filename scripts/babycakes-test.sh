e5d38e059a58a97ea149052795cdcd34cfd5b1f2
Burlen Loring <bloring@lbl.gov>
2014-05-22 12:57:30 -0700

    prevent overlapping dashboards

--- scripts/babycakes-test.sh
+++ scripts/babycakes-test.sh
@@ -10,5 +10,15 @@ fi
 source /Users/bloring/openmpi-1.6.4.sh
 export DASHBOARD_TYPE=$1
 EPOCH=`date +%s`
+if [[ -e $LOCKFILE ]]
+then
+    echo
+    echo "ERROR: Found lockfile $LOCKFILE"
+    echo "ERROR: Won't start another run while one is already running "
+    echo
+    exit -2
+fi
+touch $LOCKFILE
 ctest --timeout 120 -S /Users/bloring/visit/dashboards/babycakes-config.cmake -O ./logs/$DASHBOARD_TYPE-$EPOCH.log -V
 find /Users/bloring/visit/dashboards/logs -maxdepth 0 -name '*.log' -atime 2 -exec rm \{\} \;
+rm $LOCKFILE
--- scripts/missm-test.sh
+++ scripts/missm-test.sh
@@ -12,7 +12,18 @@ then
     exit -1
 fi
 export DASHBOARD_TYPE=$1
+LOCKFILE=lock_$DASHBOARD_TYPE
 EPOCH=`date +%s`
 cd /work2/visit-branch/dashboard
+if [[ -e $LOCKFILE ]]
+then
+    echo
+    echo "ERROR: Found lockfile $LOCKFILE"
+    echo "ERROR: Won't start another run while one is already running"
+    echo
+    exit -2
+fi
+touch $LOCKFILE
 ctest --timeout 120 -S /work2/visit-branch/dashboard/missm-config.cmake -O ./logs/$DASHBOARD_TYPE-$EPOCH.log -V
 find /work2/visit-branch/dashboard/logs -maxdepth 0 -name '*.log' -atime 2 -exec rm \{\} \;
+rm $LOCKFILE
