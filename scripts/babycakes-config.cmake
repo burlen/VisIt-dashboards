fd076bc2276de5348db48acadd054ef4a7ac02fa
Burlen Loring <bloring@lbl.gov>
2014-05-31 18:07:49 -0700

    babycakes: updates
    fix the lock file name
    make sure lock file is rm'd when run is interupted

--- scripts/babycakes-config.cmake
+++ scripts/babycakes-config.cmake
@@ -2,19 +2,20 @@ set(CTEST_SITE "babycakes.lbl.gov")
 set(CTEST_BUILD_NAME "MacOSX10.8.2-gcc4.2.1")
 set(CTEST_DASHBOARD_ROOT "/Users/bloring/visit/dashboards")
 set(CONFIG_SITE ${CTEST_DASHBOARD_ROOT}/visit-deps-all/babycakes-deps-all.cmake)
+#set(CONFIG_SITE ${CTEST_DASHBOARD_ROOT}/visit-deps-2.8/babycakes.dhcp.lbl.gov.cmake)
 set(TEST_WARNINGS "-Wall -Wno-overloaded-virtual -Wno-unused-function -Wno-sign-compare -Wno-unused-variable -Wno-write-strings -Wno-parentheses -Wno-missing-braces")
 set(INITIAL_CACHE
 "VISIT_CONFIG_SITE:STRING=${CONFIG_SITE}
 BUILD_TESTING=ON
 VISIT_BUILD_ALL_PLUGINS=ON
 VISIT_DATA_MANUAL_EXAMPLES=ON
-CMAKE_CXX_FLAGS=${TEST_WARNINGS}")
+CMAKE_CXX_FLAGS=-D_GLIBCXX_DEBUG ${TEST_WARNINGS}")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_SVN_COMMAND "/usr/bin/svn")
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
 set(CTEST_BUILD_FLAGS -j16)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(DASHBOARD_RELIABLE TRUE)
 #set(ALWAYS_UPDATE ON)
 #set(INCREMENTAL_BUILD OFF)
-#set(DASHBOARD_RELIABLE TRUE)
 include("${CTEST_DASHBOARD_ROOT}/visit-dashboard.cmake")
--- scripts/babycakes-test.sh
+++ scripts/babycakes-test.sh
@@ -8,8 +8,12 @@ then
     exit -1
 fi
 source /Users/bloring/openmpi-1.6.4.sh
+DASHROOT=/Users/bloring/visit/dashboards
+#export LD_LIBRARY_PATH=$DASHROOT/visit-deps-2.8/visit/vtk/6.1.0/darwin/:$LD_LIBRARY_PATH
+cd $DASHROOT
 export DASHBOARD_TYPE=$1
 EPOCH=`date +%s`
+LOCKFILE=lock_$DASHBOARD_TYPE
 if [[ -e $LOCKFILE ]]
 then
     echo
@@ -19,6 +23,7 @@ then
     exit -2
 fi
 touch $LOCKFILE
-ctest --timeout 120 -S /Users/bloring/visit/dashboards/babycakes-config.cmake -O ./logs/$DASHBOARD_TYPE-$EPOCH.log -V
-find /Users/bloring/visit/dashboards/logs -maxdepth 0 -name '*.log' -atime 2 -exec rm \{\} \;
+trap "rm $LOCKFILE; exit" SIGHUP SIGINT SIGTERM
+ctest --timeout 120 -S $DASHROOT/babycakes-config.cmake -O ./logs/$DASHBOARD_TYPE-$EPOCH.log -V
+find $DASHROOT/logs -maxdepth 0 -name '*.log' -atime 2 -exec rm \{\} \;
 rm $LOCKFILE
