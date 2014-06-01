152d4d0372376cca071d6ff2fd588ac454084f11
Burlen Loring <bloring@lbl.gov>
2014-05-31 18:00:10 -0700

    missmarple: updates
    move to visit 2.8 deps including vtk 6.1.
    set vtk install in ld library path
    make sure lock file is rm'd when tests are brought
    down

--- scripts/missm-config.cmake
+++ scripts/missm-config.cmake
@@ -1,14 +1,14 @@
 set(CTEST_SITE "missmarple.lbl.gov")
 set(CTEST_BUILD_NAME "Ubuntu12.04-gcc4.6.3")
 set(CTEST_DASHBOARD_ROOT "/work2/visit-branch/dashboard/")
-set(CONFIG_SITE ${CTEST_DASHBOARD_ROOT}/visit-deps-io/missmarple.cmake)
-set(TEST_WARNINGS "-Wall -Wno-unused-function -Wno-unused-variable -Wno-write-strings -Wno-parentheses -Wno-unused-but-set-variable -Wno-missing-braces -Wno-reorder -Wno-unused-result")
+set(CONFIG_SITE ${CTEST_DASHBOARD_ROOT}/visit-deps-2.8/missmarple.cmake)
+set(TEST_WARNINGS "-Wall -Wno-sign-compare -Wno-unused-function -Wno-unused-variable -Wno-write-strings -Wno-parentheses -Wno-unused-but-set-variable -Wno-missing-braces -Wno-reorder -Wno-unused-result")
 set(INITIAL_CACHE
 "VISIT_CONFIG_SITE:STRING=${CONFIG_SITE}
 BUILD_TESTING=ON
 VISIT_BUILD_ALL_PLUGINS=ON
 VISIT_DATA_MANUAL_EXAMPLES=ON
-CMAKE_CXX_FLAGS=${TEST_WARNINGS}")
+CMAKE_CXX_FLAGS=-D_GLIBCXX_DEBUG ${TEST_WARNINGS}")
 set(CTEST_BUILD_CONFIGURATION Debug)
 set(CTEST_SVN_COMMAND "/usr/bin/svn")
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
--- scripts/missm-test.sh
+++ scripts/missm-test.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -l
-module load mpich-3.0.3-R
-module load cmake-2.8.10
-module load qt-4.8.2
-export DISPLAY=:0
 if [[ $# != 1 ]]
 then
     echo
@@ -11,10 +7,15 @@ then
     echo
     exit -1
 fi
+module load mpich-3.0.3-R
+module load cmake-2.8.10
+module load qt-4.8.2
+DASHROOT=/work2/visit-branch/dashboard/
+cd $DASHROOT
+export LD_LIBRARY_PATH=$DASHROOT/visit-deps-2.8/visit/vtk/6.1.0/x86_64/lib:$LD_LIBRARY_PATH
+export DISPLAY=:0
 export DASHBOARD_TYPE=$1
 LOCKFILE=lock_$DASHBOARD_TYPE
-EPOCH=`date +%s`
-cd /work2/visit-branch/dashboard
 if [[ -e $LOCKFILE ]]
 then
     echo
@@ -24,6 +25,8 @@ then
     exit -2
 fi
 touch $LOCKFILE
+trap "rm -f $LOCKFILE; exit" SIGHUP SIGINT SIGTERM
+EPOCH=`date +%s`
 ctest --timeout 120 -S /work2/visit-branch/dashboard/missm-config.cmake -O ./logs/$DASHBOARD_TYPE-$EPOCH.log -V
 find /work2/visit-branch/dashboard/logs -maxdepth 0 -name '*.log' -atime 2 -exec rm \{\} \;
 rm $LOCKFILE
