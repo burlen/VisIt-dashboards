d8f093cd07398a1ca9489b077cd9573242d34ea4
Burlen Loring <bloring@lbl.gov>
2014-07-08 11:07:29 -0700

    rocky: initial commit of config/scripts

---
+++ scripts/rocky-test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+export DISPLAY=:0
+if [[ $# != 1 ]]
+then
+    echo
+    echo "ERROR: Usage"
+    echo "$0 Experimental|Nightly|Continuous"
+    echo
+    exit -1
+fi
+module load mpich/3.1-rc1
+DASHROOT=/work/dashboards/visit
+cd $DASHROOT
+export LD_LIBRARY_PATH=$DASHROOT/visit-deps/visit/vtk/6.1.0/x86_64/lib:$LD_LIBRARY_PATH
+export DISPLAY=:0
+export DASHBOARD_TYPE=$1
+LOCKFILE=lock_$DASHBOARD_TYPE
+if [[ -e $LOCKFILE ]]
+then
+    echo
+    echo "ERROR: Found lockfile $LOCKFILE"
+    echo "ERROR: Won't start another run while one is already running"
+    echo
+    exit -2
+fi
+touch $LOCKFILE
+trap "rm -f $LOCKFILE; exit" SIGHUP SIGINT SIGTERM
+EPOCH=`date +%s`
+ctest --timeout 120 -S ${DASHROOT}/rocky-config.cmake -O ./logs/$DASHBOARD_TYPE-$EPOCH.log -V
+find ${DASHROOT}/logs -maxdepth 0 -name '*.log' -atime 2 -exec rm \{\} \;
+rm $LOCKFILE
